a5bdc2d20e411c49184e755695165ee474246908
Property404 <dontemailmeormysoneveragain@dagans.dev>
2025-11-11 01:52:13 -0600

    sss: make testable

--- src/sss.rs
+++ src/sss.rs
@@ -10,11 +10,11 @@ const PAGE_SIZE: usize = 4096;
 #[derive(Parser)]
 struct Args {}
 
-fn main() -> Result<()> {
-    let _args = Args::parse();
-    let mut stdin = io::stdin().lock();
-    let mut stderr = io::stderr().lock();
-    let mut stdout = io::stdout().lock();
+fn stream_split(
+    mut stdin: impl Read,
+    mut stdout: impl Write,
+    mut stderr: impl Write,
+) -> Result<()> {
     let mut buf = [0u8; PAGE_SIZE];
     while let bytes = stdin.read(&mut buf)?
         && bytes != 0
@@ -22,5 +22,39 @@ fn main() -> Result<()> {
         stdout.write_all(&buf[0..bytes])?;
         stderr.write_all(&buf[0..bytes])?;
     }
+
     Ok(())
+}
+
+fn main() -> Result<()> {
+    let _args = Args::parse();
+    stream_split(io::stdin().lock(), io::stdout().lock(), io::stderr().lock())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn basic_functionality() {
+        let tvs = [
+            "",
+            "hi",
+            "foo\nbar",
+            "\n",
+            "\r",
+            "\r\nbaz\n\t",
+            "The quic\nk brown fox jamped\n over the lazy dorg\n\n",
+            "👋",
+        ];
+        for tv in tvs {
+            let stdin = Cursor::new(String::from(tv));
+            let mut stdout = Vec::<u8>::new();
+            let mut stderr = Vec::<u8>::new();
+            stream_split(stdin, &mut stdout, &mut stderr).unwrap();
+            assert_eq!(tv, String::from_utf8(stdout).unwrap());
+            assert_eq!(tv, String::from_utf8(stderr).unwrap());
+        }
+    }
 }
